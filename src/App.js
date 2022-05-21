efdee85c17efca2a28151a8dcbd3145ad7c5a0cd
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-05-21 17:16:18 +0530

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const name1 = "zubair";
+  const name2 = "Basit";
+  const [isdance, setIsdance] = useState(false);
+  const toggleDance = () => {
+    setIsdance = !isdance;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {" "}
+        New app Creation in progress...
+        <br /> By
+      </h1>
+
+      <h2 className="Names dance">{name1}</h2>
+      <p> and </p>
+      <h2 className="Names dance">{name2}</h2>
     </div>
   );
 }
