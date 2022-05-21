95de3fc5ee253d3bd042dc937483434ca1dc87bd
user2695 <zy.khan37@gmail.com>
2022-05-21 19:00:24 +0530

    Let's goooogit push!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
 
 function App() {
-  const name1 = "zubair";
-  const name2 = "Basit";
-
   return (
     <div className="App">
-      <h1>
-        {" "}
-        New app Creation in progress...
-        <br /> By
-      </h1>
-
-      <h2 className="Names dance">{name1}</h2>
-      <p> and </p>
-      <h2 className="Names dance">{name2}</h2>
+      <h1>Hello world!</h1>
     </div>
   );
 }
